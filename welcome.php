3cb916f3b1eb7115eb4b1d82e76ed0ecc9330a5b
TikhonP <tikhonp@users.noreply.github.com>
2021-11-29 00:38:00 +0300

    fixed register

--- welcome.php
+++ welcome.php
@@ -8,14 +8,15 @@ if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
     exit;
 }
 
-$sql = "SELECT username, is_staff, email FROM users WHERE id = ?";
+$sql = "SELECT id, username, is_staff, email FROM users WHERE username = ?";
 
 if ($stmt = mysqli_prepare($link, $sql)) {
-    mysqli_stmt_bind_param($stmt, "i", $_SESSION["id"]);
+    mysqli_stmt_bind_param($stmt, "s", $param_username);
+    $param_username = $_SESSION["username"];
 
     if (mysqli_stmt_execute($stmt)) {
         mysqli_stmt_store_result($stmt);
-        mysqli_stmt_bind_result($stmt, $username, $is_staff, $email);
+        mysqli_stmt_bind_result($stmt, $id, $username, $is_staff, $email);
         if (!mysqli_stmt_fetch($stmt)) {
             echo "error fetch request!";
             exit;
