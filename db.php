02b08bb57bb8f49f38244ca4017fd47aaf0455a2
TikhonP <tikhonp@users.noreply.github.com>
2021-11-28 22:04:18 +0300

    fix registration.php

--- db.php
+++ db.php
@@ -1,10 +1,6 @@
 <?php
-// Enter your Host, username, password, database below.
-// I left password empty because i do not set password on localhost.
-$conn = mysqli_connect("localhost", "rozhko40_iu4db", "oF&M0YU&", "rozhko40_iu4db");
-// Check connection
-if (mysqli_connect_errno())
-  {
+$con = mysqli_connect("localhost", "rozhko40_iu4db", "oF&M0YU&", "rozhko40_iu4db");
+if (mysqli_connect_errno()) {
   echo "Failed to connect to MySQL: " . mysqli_connect_error();
-  }
+}
 ?>
