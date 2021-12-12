280854c52e5b4ab7681dd28d300bb6d5d6fc4110
TikhonP <tikhonp@users.noreply.github.com>
2021-12-13 00:32:49 +0300

    add ajax for register request

--- register.php
+++ register.php
@@ -85,7 +85,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     if (empty(trim($_POST["email"]))) {
         $email = "";
-    } elseif (!valid_email(trim($_POST["email"]))) {
+    } elseif (valid_email(trim($_POST["email"])) == false) {
         $email_err = "Введите верный email. ";
     } else {
         $email = trim($_POST["email"]);
